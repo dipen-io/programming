07993fa95d0274541c3729697c00db87d7d3792a
Dipen Boro <borod9200@gmail.com>
2026-04-09 16:29:21 +0000

    zig basic

---
+++ zig/hello_01.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // Variables 
+    const language = "Zig";
+    const year  = 2026;
+
+    std.debug.print("Learning {s} in {}\n", .{language, year});
+
+    // basic arithmatic
+    const a = 12;
+    const b= 21;
+    std.debug.print("{} + {} = {}\n", .{a, b, a+b});
+
+    // Arrays
+    const number = [_]i32{1,2,3,4,5};
+    std.debug.print("First Number: {}\n", .{number[0]});
+    std.debug.print("Array Length: {}\n", .{number.len});
+}
