9ad705d4660ba1ccbcc9db51001ead1f55ec1920
Dipen Boro <borod9200@gmail.com>
2026-04-17 03:08:10 +0000

    adding localthread

---
+++ zig/variable/thread_local_var.zig
@@ -0,0 +1,19 @@
+// each thread work with seperate instance of the variable
+const std = @import("std");
+const assert = std.debug.assert;
+
+threadlocal var x: i32 = 1234;
+
+test "thread local storage" {
+    const thread1 = try std.Thread.spawn(.{}, testTls, .{});
+    const thread2 = try std.Thread.spawn(.{}, testTls, .{});
+    testTls();
+    thread1.join();
+    thread2.join();
+}
+
+fn testTls() void {
+    assert(x == 1234);
+    x += 1;
+    assert(x == 1235);
+}
