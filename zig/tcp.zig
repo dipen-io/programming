18f99c1907d52cc05c2da148da1f7a6196c7052f
Dipen Boro <borod9200@gmail.com>
2026-04-17 10:53:27 +0000

    tcp connection added

---
+++ zig/tcp.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const net = std.Io.net;
+
+const port: u32 = 5800;
+
+pub fn main() !void {
+    var threaded: std.Io.Threaded = .init_single_threaded;
+    defer threaded.deinit();
+
+    const io = threaded.io();
+
+    const addr = try net.IpAddress.parseIp4("127.0.0.1", port);
+
+    var server = try addr.listen(io, .{
+        .kernel_backlog = 128,
+        .reuse_address = true,
+    });
+    defer server.deinit(io);
+
+    std.debug.print("Listening on 127.0.0.1: {d}\n", .{port});
+    while (true) {
+        var conn = try server.accept(io);
+        defer conn.close(io);
+
+        std.debug.print("Client connected: {} \n", .{conn});
+    }
+
+}
