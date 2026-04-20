c53ee9badcfea3da806057e7a1fc5c6cefc35223
Dipen Boro <borod9200@gmail.com>
2026-04-20 06:40:24 +0000

    memory allocator_01

--- zig/allocator_01.zig
+++ zig/allocator_01.zig
@@ -1,16 +1,14 @@
 const std = @import("std");
 
-pub fn main() !void {
+pub fn main(init: std.process.Init) !void {
     // Manual memory management with allocators
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    // allocator is an interface
-    const allocator = gpa.allocator();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa = init.arena.allocator();
 
     // Allocate memory
     // here we allocate memeroy
-    const message = try allocator.alloc(u8, 100);
-    defer allocator.free(message); // Explicit cleanup
+    const message = try gpa.alloc(u8, 100);
+    defer gpa.free(message);
 
-    std.debug.print("Memory allocated!\n", .{});
+    std.debug.print("Memory allocated! \n", .{});
 }
