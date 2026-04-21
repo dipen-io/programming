3f8501bc749b69075c3af359561e37ecfe63f393
Dipen Boro <borod9200@gmail.com>
2026-04-21 11:44:15 +0000

    return or void same

--- zig/chapter/void.zig
+++ zig/chapter/void.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
+fn empty() void {}
+fn nothing() void {
+    return;
+}
+
 pub fn main() !void {
     const n_x = {}; // void
     const x = n_x; // void
     assert(n_x == x);
     std.debug.print("{}\n", .{x});
     std.debug.print("{}\n", .{n_x});
+
+    const void_x = empty();
+    const void_y = nothing();
+    std.debug.print("Returned empty:{} , nothing:{}\n", .{ void_x, void_y });
 }
