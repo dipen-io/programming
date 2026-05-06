881d6812fec923236c12e164f6bdd60b85d04eee
Dipen Boro <borod9200@gmail.com>
2026-05-06 20:28:44 +0530

    zig function

---
+++ zig/function/fuc.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+// simple function no param/return
+fn greet() void {
+    std.debug.print("greeting...\n", .{});
+}
+// with param
+fn greetPerson(name: []const u8) void {
+    std.debug.print("greeting...{s}\n", .{name});
+}
+// with return value
+fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+pub fn main() void {
+    greet();
+    const a = 10;
+    const b = 10;
+    greetPerson("dinesh");
+    std.debug.print("Adding {} + {} = {}", .{ a, b, add(a, b) });
+}
