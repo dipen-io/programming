739c8ed04f89fdf53ddcda255ab1087666fcba3f
Dipen Boro <borod9200@gmail.com>
2026-09-01 18:56:11 +0530

    primitive

---
+++ java/primitive/main.java
@@ -0,0 +1,28 @@
+/*
+    Boolean (true / false) default -> false 
+
+*/
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        // 1.Boolean
+        boolean flag = true;
+        System.out.println(flag);
+
+        // 2.byte
+        byte range;
+        range = 100;
+        System.out.println(range);
+
+        // 3. short
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);
+
+        // 3. string
+        String myString = "Java Programming";
+        System.out.println(myString);
+    }
+}
